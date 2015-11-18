44465ca5f471a123fa2ff1c2da2d9530927ca224
rodolphofr <rodolpho.frodrigues@gmail.com>
2015-11-17 23:40:19 -0200

    remove import;

--- olympic-games/test/br/com/competition/domain/competition/OneHundredMetersTest.java
+++ olympic-games/test/br/com/competition/domain/competition/OneHundredMetersTest.java
@@ -1,9 +1,9 @@
 package br.com.competition.domain.competition;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import br.com.competition.domain.Runner;
