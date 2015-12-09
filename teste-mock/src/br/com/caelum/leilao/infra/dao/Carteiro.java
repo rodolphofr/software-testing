0cfeb3c43105e318e73b1e500718595e006bcbcf
Rodolpho <rodolrod@inmetrics.com.br>
2015-12-08 23:04:49 -0200

    novos métodos mock;

--- teste-mock/src/br/com/caelum/leilao/infra/dao/Carteiro.java
+++ teste-mock/src/br/com/caelum/leilao/infra/dao/Carteiro.java
@@ -2,6 +2,6 @@ package br.com.caelum.leilao.infra.dao;
 
 import br.com.caelum.leilao.dominio.Leilao;
 
-public interface EnviadorDeEmail {
+public interface Carteiro {
 	void envia(Leilao leilao);
 }
